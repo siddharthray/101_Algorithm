5601da7768421067cd79f8739617885c6df15c23
siddharth <siddharth@siddharths-MacBook-Pro.local>
2018-09-10 22:42:06 +0530

    added optimised function

--- algo_3/algo_3.ts
+++ algo_3/algo_3.ts
@@ -15,4 +15,19 @@ function addBorder(picture: string[]): any {
 
 }
 
+function addBorder2(picture: string[]): any {
+    
+    let wall = '*'.repeat(picture[0].length + 2);
+
+    picture.unshift(wall);
+    picture.push(wall);
+
+    for (let i = 0; i < picture.length - 1; i++) {
+        picture[i] = '*'.concat(picture[i], '*');
+    }
+    return picture;
+
+}
+
 console.log(addBorder(["abc", "def"]));
+console.log(addBorder2(["abc", "def"]));
